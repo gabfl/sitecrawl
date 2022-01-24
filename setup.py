01f221ef5648743b7b23da384960db6a01f95394
gab <gabrielbordeaux@gmail.com>
2022-01-24 18:59:56 -0500

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+import pypandoc
 
 setup(
     name='sitecrawl',
     version='1.0',
     description='Simple Python3 module to crawl a website and extract URLs',
-    long_description=long_description,
+    long_description=pypandoc.convert_file('README.md', 'rst'),
     author='Gabriel Bordeaux',
     author_email='pypi@gab.lc',
     url='https://github.com/gabfl/sitecrawl',
