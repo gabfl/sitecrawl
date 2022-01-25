a9ac99a045e119ec197e85ea144a0382c5a451d6
Gabriel Bordeaux <gabrielbordeaux@gmail.com>
2022-01-24 21:14:50 -0500

    add missing import

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 try:
+    import pypandoc
     long_description = pypandoc.convert_file('README.md', 'rst')
 except(IOError, ImportError):
     long_description = open('README.md').read()
