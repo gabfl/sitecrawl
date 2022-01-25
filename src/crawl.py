db05afa4ce4cba5c1f088ef5e5b3be37117ec332
gab <gabrielbordeaux@gmail.com>
2022-01-24 22:46:13 -0500

    Remove br compression

--- src/crawl.py
+++ src/crawl.py
@@ -9,7 +9,7 @@ valid_content_types = [
 crawling_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
     "accept-language": "en-US,en;q=0.8",
-    "Accept-Encoding": "br, gzip, deflate",
+    "Accept-Encoding": "gzip, deflate",
     "Accept": "test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Connection": "keep-alive",
     "Upgrade-Insecure-Requests": "1",
